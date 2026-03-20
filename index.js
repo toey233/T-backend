301323d9940093a454fbdaa84228f3a7828bbf07
toey233 <your.email@example.com>
2026-03-20 09:35:48 +0700

    up

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'https://k-frontend-iota.vercel.app',
+  origin: process.env.FRONTEND_URL || 'https://t-frontend-nine.vercel.app',
   credentials: true
 }));
 
